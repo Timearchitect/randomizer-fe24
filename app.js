6f0f757ce82b5a835ab67ab57815c2a9d0fdd65f
Alrik He <alrik_he@hotmail.com>
2025-03-25 10:55:08 +0100

    läsa

---
+++ app.js
@@ -0,0 +1,9 @@
+var fs = require('fs'); 
+fs.readFile('lista.json' , "utf8", function(err, data) {
+    console.log(data);
+    let jsonObj = JSON.parse(data) 
+    console.log(jsonObj);
+  /*   res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data); */
+   // return res.end();
+  });
