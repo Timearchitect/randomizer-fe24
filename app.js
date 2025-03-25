a965ca9f47cc853c55d6a642500a0f9cd0613afa
Alrik He <alrik_he@hotmail.com>
2025-03-25 10:57:34 +0100

    write

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ fs.readFile('lista.json' , "utf8", function(err, data) {
   /*   res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data); */
    // return res.end();
+
+   jsonObj = JSON.stringify(jsonObj)
+   fs.writeFile('grupper.json', jsonObj, function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  }); 
+
   });
